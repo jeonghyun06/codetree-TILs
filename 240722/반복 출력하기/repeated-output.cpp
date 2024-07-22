f2128982e4bd7cfef09db06f2e23db51ca791864
LeeJeongHyun <76839646+jeonghyun06@users.noreply.github.com>
2024-07-22 16:29:50 +0900

    Title: 반복 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240722/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void func(int n){
+    for(int i=0;i<n;i++){
+        cout<<"12345^&*()_\n";
+    }
+}
+
+int main() {
+    int N;
+    cin>>N;
+    func(N);
+    return 0;
+}
