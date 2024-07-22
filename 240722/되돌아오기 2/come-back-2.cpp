b49e77cc1a36343a26af8319990a173c6c650fbd
LeeJeongHyun <76839646+jeonghyun06@users.noreply.github.com>
2024-07-22 16:22:36 +0900

    Title: 되돌아오기 2, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 240722/되돌아오기 2/come-back-2.cpp
+++ 240722/되돌아오기 2/come-back-2.cpp
@@ -16,7 +16,7 @@ int main() {
             y += dy[dir];
         }
         else if(s[i]=='L'){
-            dir = (dir-1) % 4;
+            dir = (dir+3) % 4;
         }
         else if(s[i]=='R'){
             dir = (dir+1) % 4;
