0bc944d18ec4c0151ffe574dab8e94445bc4e0ac
LeeJeongHyun <76839646+jeonghyun06@users.noreply.github.com>
2024-07-23 18:00:04 +0900

    Title: 가운데에서 시작하여 빙빙 돌기, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

--- 240723/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.cpp
+++ 240723/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int dx[4] = {1, 0, -1, 0};
+int dx[4] = {-1, 0, 1, 0};
 int dy[4] = {0, -1, 0, 1};
 int box[102][102];
 
@@ -9,32 +9,30 @@ int main() {
     int n;
     cin>>n;
 
-    int cnt = 1;
-    int x = n/2;
-    int y = n/2;
+    for(int i=0;i<=n+1;i++){
+        box[0][i]=-1;
+        box[i][0]=-1;
+        box[n+1][i]=-1;
+        box[i][n+1]=-1;
+    }
+
+    int cnt = 25;
+    int x = n;
+    int y = n;
     int dir = 0;
-    int num=1, check=2;
 
-    box[y][x] = cnt++;
-    x += dx[dir];
-    y += dy[dir];
-
-    while(cnt<=n*n){
-        box[y][x] = cnt++;
-        check++;
-        if(num <= check) {
-            if(num!=n){
-                num++;
-            }
-            check=1;
+    int num = 1;
+    while(cnt>0){
+        box[y][x] = cnt--;
+        if(box[y+dy[dir]][x+dx[dir]]!= 0){
             dir = (dir+1)%4;
         }
         x += dx[dir];
         y += dy[dir];
     }
     
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
             cout<<box[i][j]<<' ';
         }
         cout<<'\n';
