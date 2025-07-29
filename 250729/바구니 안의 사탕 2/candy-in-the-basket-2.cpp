b184cd6a4f6d737f2ff124e408821f0ce0a8aca1
LeeJeongHyeon <76839646+jeonghyun06@users.noreply.github.com>
2025-07-29 10:50:14 +0900

    Title: 바구니 안의 사탕 2, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250729/바구니 안의 사탕 2/candy-in-the-basket-2.cpp
+++ 250729/바구니 안의 사탕 2/candy-in-the-basket-2.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int N, K, cp, bp, maxSum = 0;
-int candy[100];
+int candy[101];
 
 int main() {
     cin >> N >> K;
@@ -13,9 +13,18 @@ int main() {
         candy[bp] += cp;
     }
 
-    for(int c = K; c < 100-K ; c++){
+    for(int c = K + 1; c < 101-K ; c++){
         int curSum = 0;
         for(int i = c-K; i <= c + K; i++){
+            curSum += candy[i];
+        }
+        maxSum = max(maxSum, curSum);
+    }
+
+    // 2k+1>=100 -> 전체합
+    if (2*K + 1 >=100){
+        int curSum = 0;
+        for(int i=1;i<101;i++){
             curSum += candy[i];
         }
         maxSum = max(maxSum, curSum);
