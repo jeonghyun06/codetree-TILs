610efa4c80f334b630b2846da03a6bb6134a68dd
LeeJeongHyeon <76839646+jeonghyun06@users.noreply.github.com>
2025-07-30 10:29:03 +0900

    Title: 한 가지로 열리는 자물쇠, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250730/한 가지로 열리는 자물쇠/one-way-lock.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+int arr[3];
+
+int main() {
+    cin >> N;
+    cin >> arr[0] >> arr[1] >> arr[2];
+
+    // 계산
+    int cnt[3]{};
+    for(int i = 0; i < 3; i++){
+        cnt[i] += max(4 - arr[i], 0);
+        cnt[i] += max(arr[i] - 3, 0);
+    }
+
+    cout << 6*6*6 - cnt[0] * cnt[1] * cnt[2];
+
+    return 0;
+}
