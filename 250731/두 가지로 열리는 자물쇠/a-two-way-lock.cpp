ae4234314ca8e607951ea76114b41274a94a8652
LeeJeongHyeon <76839646+jeonghyun06@users.noreply.github.com>
2025-07-31 11:43:09 +0900

    Title: 두 가지로 열리는 자물쇠, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250731/두 가지로 열리는 자물쇠/a-two-way-lock.cpp
+++ 250731/두 가지로 열리는 자물쇠/a-two-way-lock.cpp
@@ -13,10 +13,22 @@ int main() {
     cin >> b[0] >> b[1] >> b[2];
     
 
-    int ma = pow(N-5,3)*2;
+    int ma = 5*5*5*2;
     for(int i=0;i<3;i++){
-        // cout<<max(0,5-(abs(a[i]-b[i]) % N))<<" ";
-        cnt[i] = max(0,5-(abs(a[i]-b[i]) % N));
+        // cnt[i] += max(N - 2 - arr[i], 0);
+        // cnt[i] += max(arr[i] - 3, 0);
+        int t1, t2;
+        if (a[i]>b[i]){
+            t1 = ((a[i] + 2) % N - (b[i] - 2 + N ) % N + N )% N + 1;
+            t2 = ((a[i] - 2 + N) % N - (b[i] + 2) % N + N )% N + 1;
+        }
+        else{
+            t1 = ((b[i] + 2) % N - (a[i] - 2 + N ) % N + N )% N + 1;
+            t2 = ((b[i] - 2 + N) % N - (a[i] + 2) % N + N )% N + 1;
+        }
+        cnt[i] += t1<=5 ? t1: 0;
+        cnt[i] += t2<=5 ? t2: 0;
+        cnt[i] = min(cnt[i], 5);
     }
 
     cout<<ma - cnt[0]*cnt[1]*cnt[2];
