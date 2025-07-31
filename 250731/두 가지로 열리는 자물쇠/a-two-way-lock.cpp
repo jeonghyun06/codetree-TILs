471edba70ee9f2f88dc8e6f31124e2cb9181eec3
LeeJeongHyeon <76839646+jeonghyun06@users.noreply.github.com>
2025-07-31 11:46:02 +0900

    Title: 두 가지로 열리는 자물쇠, Time: 9ms, Memory: 0MB, Status: Passed - Codetree

--- 250731/두 가지로 열리는 자물쇠/a-two-way-lock.cpp
+++ 250731/두 가지로 열리는 자물쇠/a-two-way-lock.cpp
@@ -1,36 +1,33 @@
 #include <iostream>
-#include <cmath>
+
 using namespace std;
 
-int N;
-int a[3];
-int b[3];
-int cnt[3];
+int n;
+int a, b, c;
+int a2, b2, c2;
 
 int main() {
-    cin >> N;
-    cin >> a[0] >> a[1] >> a[2];
-    cin >> b[0] >> b[1] >> b[2];
+    // 입력
+    cin >> n;
+    cin >> a >> b >> c;
+	cin >> a2 >> b2 >> c2;
     
-
-    int ma = 5*5*5*2;
-    for(int i=0;i<3;i++){
-        // cnt[i] += max(N - 2 - arr[i], 0);
-        // cnt[i] += max(arr[i] - 3, 0);
-        int t1, t2;
-        if (a[i]>b[i]){
-            t1 = ((a[i] + 2) % N - (b[i] - 2 + N ) % N + N )% N + 1;
-            t2 = ((a[i] - 2 + N) % N - (b[i] + 2) % N + N )% N + 1;
-        }
-        else{
-            t1 = ((b[i] + 2) % N - (a[i] - 2 + N ) % N + N )% N + 1;
-            t2 = ((b[i] - 2 + N) % N - (a[i] + 2) % N + N )% N + 1;
-        }
-        cnt[i] += t1<=5 ? t1: 0;
-        cnt[i] += t2<=5 ? t2: 0;
-        cnt[i] = min(cnt[i], 5);
-    }
-
-    cout<<ma - cnt[0]*cnt[1]*cnt[2];
+    // 모든 조합을 다 만들어 봅니다.
+    int cnt = 0;
+    for(int i = 1; i <= n; i++)
+        for(int j = 1; j <= n; j++)
+            for(int k = 1; k <= n; k++) {
+                // 모든 자리가 주어진 첫 조합과의 거리가 2 이내인지 확인합니다.
+                if((abs(a - i) <= 2 || abs(a - i) >= n - 2) && (abs(b - j) <= 2 || abs(b - j) >= n - 2) && 
+                   (abs(c - k) <= 2 || abs(c - k) >= n - 2))
+                    cnt++;
+				
+				// 모든 자리가 주어진 두번째 조합과의 거리가 2 이내인지 확인합니다.
+                else if((abs(a2 - i) <= 2 || abs(a2 - i) >= n - 2) && (abs(b2 - j) <= 2 || abs(b2 - j) >= n - 2) && 
+                   (abs(c2 - k) <= 2 || abs(c2 - k) >= n - 2))
+                    cnt++;
+            }
+    
+    cout << cnt;
     return 0;
 }
