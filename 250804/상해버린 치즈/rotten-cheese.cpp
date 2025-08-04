9ec247303c711cb30ef36f8ebbaf232be614b9c5
LeeJeongHyeon <76839646+jeonghyun06@users.noreply.github.com>
2025-08-04 11:31:45 +0900

    Title: 상해버린 치즈, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250804/상해버린 치즈/rotten-cheese.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int N, M, D, S, maxCnt;
+int p[1000], m[1000], t[1000];
+pair<int, int> sick[50];
+
+int main() {
+    cin >> N >> M >> D >> S;
+
+    for (int i = 0; i < D; i++) {
+        cin >> p[i] >> m[i] >> t[i]; // 누가 어떤치즈 언제 먹었는지
+    }
+
+    for (int i = 0; i < S; i++) {
+        cin >> sick[i].first>> sick[i].second; // 누가 언제 아팠는지
+    }
+    
+    for (int i = 1; i <= M; i++){
+        vector<pair<int, int>> eat;
+        for(int j = 0; j < D; j++){
+            if(m[j]==i) eat.push_back({p[j], t[j]});
+        }
+
+        int check = 0;
+        for(int j = 0; j < S; j++){
+            for(int k = 0; k < eat.size(); k++){
+                if(sick[j].first==eat[k].first && sick[j].second > eat[k].second) {
+                    check++;
+                    break;
+                }
+            }
+        }
+
+        if(S == check){
+            maxCnt = max((int)eat.size(), maxCnt);
+        }
+    }
+
+    cout<<maxCnt;
+    return 0;
+}
