0e42d520f3b0cc5eed72632da2d78ec09cfa9808
LeeJeongHyeon <76839646+jeonghyun06@users.noreply.github.com>
2025-08-04 12:18:21 +0900

    Title: 상해버린 치즈, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250804/상해버린 치즈/rotten-cheese.cpp
+++ 250804/상해버린 치즈/rotten-cheese.cpp
@@ -4,12 +4,12 @@
 using namespace std;
 
 int N, M, D, S, maxCnt;
-int p[1000], m[1000], t[1000];
+pair<int, pair<int, int>> cheese[1000];
 pair<int, int> sick[50];
 
-bool twice( vector<pair<int, int>> vec, int k){
-    for(int i = 0; i < vec.size(); i++){
-        if(vec[i].first==k) return true;
+bool twice(vector<pair<int, int>> vec, int k) {
+    for (int i = 0; i < vec.size(); i++) {
+        if (vec[i].first == k) return true;
     }
     return false;
 }
@@ -18,34 +18,36 @@ int main() {
     cin >> N >> M >> D >> S;
 
     for (int i = 0; i < D; i++) {
-        cin >> p[i] >> m[i] >> t[i]; // 누가 어떤치즈 언제 먹었는지
+        cin >> cheese[i].first >> cheese[i].second.first >> cheese[i].second.second; // 누가 어떤치즈 언제 먹었는지
     }
 
     for (int i = 0; i < S; i++) {
-        cin >> sick[i].first>> sick[i].second; // 누가 언제 아팠는지
+        cin >> sick[i].first >> sick[i].second; // 누가 언제 아팠는지
     }
+    sort(cheese, cheese + D);
     
-    for (int i = 1; i <= M; i++){
+    for (int i = 1; i <= M; i++) {
         vector<pair<int, int>> eat;
-        for(int j = 0; j < D; j++){
-            if(m[j]==i && !twice(eat, p[j])) eat.push_back({p[j], t[j]});
+        for (int j = 0; j < D; j++) {
+            if (cheese[j].second.first == i && !twice(eat, cheese[j].first))
+                eat.push_back({ cheese[j].first, cheese[j].second.second });
         }
 
         int check = 0;
-        for(int j = 0; j < S; j++){ // 아픈 사례
-            for(int k = 0; k < eat.size(); k++){ //먹은 애들
-                if(sick[j].first==eat[k].first && sick[j].second > eat[k].second) {
+        for (int j = 0; j < S; j++) { // 아픈 사례
+            for (int k = 0; k < eat.size(); k++) { //먹은 애들
+                if (sick[j].first == eat[k].first && sick[j].second > eat[k].second) {
                     check++;
                     break;
                 }
             }
         }
 
-        if(S == check){
+        if (S == check) {
             maxCnt = max((int)eat.size(), maxCnt);
         }
     }
 
-    cout<<maxCnt;
+    cout << maxCnt;
     return 0;
 }
