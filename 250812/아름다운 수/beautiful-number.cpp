f3e5494bb92b28d9c6826f1a20fc9915476dc7c8
LeeJeongHyeon <76839646+jeonghyun06@users.noreply.github.com>
2025-08-12 17:41:08 +0900

    Title: 아름다운 수, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250812/아름다운 수/beautiful-number.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int n, cnt;
+vector<int> beutiful;
+
+void backtracking(int x){
+    if(x==n){
+        cnt++;
+        return;
+    }
+    for(int i = 1;i<=n;i++){
+        if(x + i>n) continue;
+        for(int j=0;j<i;j++){
+            beutiful.push_back(i);
+        }
+        backtracking(x + i);
+        for(int j=0;j<i;j++){
+            beutiful.pop_back();
+        }
+    }
+
+}
+
+int main() {
+    cin >> n;
+
+    backtracking(0);
+    cout<<cnt;
+
+    return 0;
+}
