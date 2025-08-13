9ca6789a72f47b3eab05c5c52762bb005630bb28
LeeJeongHyeon <76839646+jeonghyun06@users.noreply.github.com>
2025-08-13 11:17:04 +0900

    Title: 강력한 폭발, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250813/강력한 폭발/strong-explosion.cpp
+++ 250813/강력한 폭발/strong-explosion.cpp
@@ -9,30 +9,22 @@ int dy[3][4] = {{2, 1, -1, -2},
                 {1, 0, -1, 0},
                 {1, 1, -1, -1}};
 
-int n, cntMax;
+int n, cntMax, bombCnt;
 int grid[20][20];
 int boom[20][20];
 
-void backtracing(int x, int y);
-
-void bomb(int b, int x, int y){
+void bomb(int b, int x, int y, int check){
     for(int i = 0; i < 4; i++){
         int cx = x + dx[b][i];
         int cy = y + dy[b][i];
         if(cx < 0  || cy < 0 || cx >= n || cy >=n) continue;
-        boom[cx][cy]++;
-    }
-    backtracing(x+1, y+1);
-    for(int i = 0; i < 4; i++){
-        int cx = x + dx[b][i];
-        int cy = y + dy[b][i];
-        if(cx < 0  || cy < 0 || cx >= n || cy >=n) continue;
-        boom[cx][cy]--;
+        boom[cx][cy] += check;
     }
 }
 
-void backtracing(int x, int y){
-    if(x==n && y==n){
+void backtracing(int x, int y, int n){
+    if(bombCnt<=n){
+        cout<<x<<' '<<y;
         int cnt=0;
         for(int i = 0; i < n; i++){
             for(int j  = 0; j < n; j++) {
@@ -45,13 +37,19 @@ void backtracing(int x, int y){
     for(int i = x;  i < n; i++){
         for(int j = y; j < n; j++){
             if(grid[i][j]==1) {
+                grid[i][j]--;
                 boom[i][j]++;
-                bomb(1, i, j);
-                bomb(2, i, j);
-                bomb(3, i, j);
+                for(int k = 0; k < 3; k++){
+                    bomb(k, i, j, 1);
+                    backtracing(i, j, n + 1);
+                    bomb(k, i, j, -1);
+
+                }
+                grid[i][j]++;
             }
         }
     }
+    
 }
 
 int main() {
@@ -60,10 +58,10 @@ int main() {
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             cin >> grid[i][j];
+            bombCnt += grid[i][j];
         }
     }
-
-    backtracing(0,0);
+    backtracing(0,0, 0);
 
     cout<<cntMax;
 
