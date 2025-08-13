a6a49c7d006ebecdf0da62ce7750a983bedef294
LeeJeongHyeon <76839646+jeonghyun06@users.noreply.github.com>
2025-08-13 13:51:43 +0900

    Title: 강력한 폭발, Time: 1087ms, Memory: 0MB, Status: Time Limit Exceed - Codetree

--- 250813/강력한 폭발/strong-explosion.cpp
+++ 250813/강력한 폭발/strong-explosion.cpp
@@ -1,55 +1,49 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-int dx[3][4] = {{0, 0, 0, 0},
+int dx[3][4] = { {2, 1, -1, -2},
                 {0, 1, 0, -1},
-                {-1, 1, -1, 1}};
+                {-1, 1, -1, 1} };
 
-int dy[3][4] = {{2, 1, -1, -2},
+int dy[3][4] = { {0, 0, 0, 0},
                 {1, 0, -1, 0},
-                {1, 1, -1, -1}};
+                {1, 1, -1, -1} };
 
-int n, cntMax, bombCnt;
+int n, cntMax;
 int grid[20][20];
 int boom[20][20];
+vector<pair<int, int>> bombPos;
 
-void bomb(int b, int x, int y, int check){
-    for(int i = 0; i < 4; i++){
+void bomb(int b, int x, int y, int check) {
+    for (int i = 0; i < 4; i++) {
         int cx = x + dx[b][i];
         int cy = y + dy[b][i];
-        if(cx < 0  || cy < 0 || cx >= n || cy >=n) continue;
+        if (cx < 0 || cy < 0 || cx >= n || cy >= n) continue;
         boom[cx][cy] += check;
-    }
+    }   
 }
 
-void backtracing(int x, int y, int n){
-    if(bombCnt<=n){
-        cout<<x<<' '<<y;
-        int cnt=0;
-        for(int i = 0; i < n; i++){
-            for(int j  = 0; j < n; j++) {
-                if(boom[i][j]) cnt++;
+void backtracing(int cnt) {
+    if (bombPos.size() == cnt) {
+        cnt = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (boom[i][j]) cnt++;
             }
         }
         cntMax = max(cnt, cntMax);
         return;
     }
-    for(int i = x;  i < n; i++){
-        for(int j = y; j < n; j++){
-            if(grid[i][j]==1) {
-                grid[i][j]--;
-                boom[i][j]++;
-                for(int k = 0; k < 3; k++){
-                    bomb(k, i, j, 1);
-                    backtracing(i, j, n + 1);
-                    bomb(k, i, j, -1);
-
-                }
-                grid[i][j]++;
-            }
+    for(int i = cnt; i < bombPos.size(); i++){
+        boom[bombPos[i].first][bombPos[i].second]++;
+        for (int k = 0; k < 3; k++) {
+            bomb(k, bombPos[i].first, bombPos[i].second, 1);
+            backtracing(cnt + 1);
+            bomb(k, bombPos[i].first, bombPos[i].second, -1);
         }
+        boom[bombPos[i].first][bombPos[i].second]--;
     }
-    
 }
 
 int main() {
@@ -58,12 +52,12 @@ int main() {
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             cin >> grid[i][j];
-            bombCnt += grid[i][j];
+            if (grid[i][j]) bombPos.push_back({ i, j });
         }
     }
-    backtracing(0,0, 0);
+    backtracing(0);
 
-    cout<<cntMax;
+    cout << cntMax;
 
     return 0;
 }
