8f24570de819ca8f51782e2aa0fdf21fb896cf96
LeeJeongHyeon <76839646+jeonghyun06@users.noreply.github.com>
2025-08-14 11:02:10 +0900

    Title: 겹치지 않게 선분 고르기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250814/겹치지 않게 선분 고르기/select-segments-without-overlap.cpp
+++ 250814/겹치지 않게 선분 고르기/select-segments-without-overlap.cpp
@@ -1,35 +1,67 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <tuple>
+
+#define MAX_N 15
+
 using namespace std;
 
-int n, maxCnt;
-vector<pair<int, int>> lines;
-vector<int> selected_lines;
+int n;
+pair<int, int> segments[MAX_N];
 
-void backtracking(int x,int last, int cnt) {
-    maxCnt = max(maxCnt, cnt);
-    for (int i = x; i < n; i++) {
-        if (last < lines[i].first) {
-            selected_lines.push_back(i);
-            backtracking(i+1, lines[i].second, cnt + 1);
-            selected_lines.pop_back();
-        }
-    }
+int ans;
+vector<pair<int, int> > selected_segs;
+
+bool Overlapped(pair<int, int> seg1, pair<int, int> seg2) {
+    int ax1, ax2;
+    tie(ax1, ax2) = seg1;
+
+    int bx1, bx2;
+    tie(bx1, bx2) = seg2;
+
+    // 두 선분이 겹치는지 여부는
+    // 한 점이 다른 선분에 포함되는 경우로 판단 가능합니다. 
+    return (ax1 <= bx1 && bx1 <= ax2) || (ax1 <= bx2 && bx2 <= ax2) ||
+           (bx1 <= ax1 && ax1 <= bx2) || (bx1 <= ax2 && ax2 <= bx2);
+}
+
+bool Possible() {
+    // 단 한쌍이라도 선분끼리 겹치면 안됩니다.
+	for(int i = 0; i < (int)selected_segs.size(); i++)
+		for(int j = i + 1; j < (int)selected_segs.size(); j++)
+            if(Overlapped(selected_segs[i], selected_segs[j]))
+                return false;
+
+	return true;
+}
+
+void FindMaxSegments(int cnt) {
+	if(cnt == n) {
+		if(Possible())
+			ans = max(ans, (int) selected_segs.size());
+		return;
+	}
+    
+    //넣었을 때
+	selected_segs.push_back(segments[cnt]);
+	FindMaxSegments(cnt + 1);
+    //안 넣었을 때
+	selected_segs.pop_back();
+	FindMaxSegments(cnt + 1);
 }
 
 int main() {
-    cin >> n;
-    lines.resize(n);
-
-    for (int i = 0; i < n; i++) {
-        cin >> lines[i].first >> lines[i].second;
+	cin >> n;
+	
+	for(int i = 0; i < n; i++) {
+        int x1, x2;
+        cin >> x1 >> x2;
+        segments[i] = make_pair(x1, x2);
     }
 
-    sort(lines.begin(), lines.end());
-
-    backtracking(0,-1, 0);
-    cout << maxCnt;
-
-    return 0;
+	FindMaxSegments(0);
+	
+	cout << ans;
+	return 0;
 }
