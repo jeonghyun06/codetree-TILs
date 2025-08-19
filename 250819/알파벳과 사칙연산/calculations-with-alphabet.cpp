71397b8de65769e9492a22d9e96816908c615fb2
LeeJeongHyeon <76839646+jeonghyun06@users.noreply.github.com>
2025-08-19 17:44:19 +0900

    Title: 알파벳과 사칙연산, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250819/알파벳과 사칙연산/calculations-with-alphabet.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+string expression;
+vector<int> alpha;
+int maxRslt;
+void cal(int cnt){
+    if(cnt==6){
+        int rslt = alpha[expression[0]-'a'];
+        for(int i=2;i<expression.length(); i += 2){
+            if(expression[i-1]=='+'){
+                rslt += alpha[expression[i]-'a'];
+            }
+            else if(expression[i-1]=='-'){
+                rslt -= alpha[expression[i]-'a'];
+            }
+            else if(expression[i-1]=='*'){
+                rslt *= alpha[expression[i]-'a'];
+            }
+        }
+        maxRslt = max(rslt, maxRslt);
+        return;
+    }
+    for(int i=1;i<=4;i++){
+        alpha.push_back(i);
+        cal(cnt + 1);
+        alpha.pop_back();
+    }
+}
+
+int main() {
+    cin >> expression;
+
+    cal(0);
+
+    cout<<maxRslt;
+
+    return 0;
+}
