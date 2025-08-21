63e30399bb2d3ac924ef231ed69387aff53780a1
LeeJeongHyeon <76839646+jeonghyun06@users.noreply.github.com>
2025-08-21 10:54:53 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 9ms, Memory: 0MB, Status: Passed - Codetree

--- 250821/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.cpp
+++ 250821/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.cpp
@@ -7,16 +7,17 @@ int visited[101][101];
 int isPossible;
 
 void path(int x, int y){
+    if(isPossible) return;
     if(x==N-1 && y==N-1){
         isPossible=1;
         return;
     }
     if(x + 1 < N && grid[x + 1][y] == 1 && !visited[x + 1][y]) {
-        visited[x + 1][y];
+        visited[x + 1][y]++;
         path(x + 1, y);
     }
     if(y + 1 < N && grid[x][y + 1] == 1 && !visited[x][y + 1]) {
-        visited[x][y + 1];
+        visited[x][y + 1]++;
         path(x, y + 1);
     }
 }
