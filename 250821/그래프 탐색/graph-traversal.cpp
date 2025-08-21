cbb80f84a3435dc3ff38bcaa911774f33504f03e
LeeJeongHyeon <76839646+jeonghyun06@users.noreply.github.com>
2025-08-21 10:31:19 +0900

    Title: 그래프 탐색, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250821/그래프 탐색/graph-traversal.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int N, M;
+int vertex[1001][1001];
+int visited[1001];
+int node[1001];
+
+void dfs(int curNode){
+    for(int i = 1 ; i <= N; i++){
+        if(!visited[i] && vertex[curNode][i]){
+            visited[i]++;
+            dfs(i);
+        }
+    }
+}
+
+int main() {
+    int x, y;
+
+    cin >> N >> M;
+    for(int i = 0; i < M; i++){
+        cin >> x >> y;
+        vertex[x][y] = 1;
+        vertex[y][x] = 1;
+    }
+
+    dfs(1);
+
+    int cnt = 0;
+    for(int i = 1; i <= N; i++){
+        if(visited[i]) cnt++;
+    }
+    cout<<cnt - 1;
+
+    return 0;
+}
