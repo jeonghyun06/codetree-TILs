5fd3f8d331ae81dea5e1ec34af163656208e89ca
LeeJeongHyeon <76839646+jeonghyun06@users.noreply.github.com>
2025-09-24 10:48:22 +0900

    Title: 서로 다른 BST 개수 세기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250924/서로 다른 BST 개수 세기/number-of-unique-bst.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+
+int main() {
+    cin >> n;
+
+    int dp[20]{};
+
+    dp[0] = 1;
+    dp[1] = 1;
+    // dp[2] = dp[1] * 2;
+    // dp[3] = dp[2] * dp[0] * 2 + dp[1] * dp[1];
+    // dp[4] = dp[3] * dp[0] * 2 + dp[2] * dp[1] * 2;
+    // dp[5] = dp[4] * dp[0] * 2 + dp[3] * dp[1] * 2 + dp[2] * dp[2];
+    // dp[6] = dp[5] * dp[0] * 2 + dp[4] * dp[1] * 2 + dp[3] * dp[2] * 2;
+
+    for(int i = 2; i <= n; i++){
+        for(int j = i - 1; j >= i / 2; j--){
+            dp[i] += dp[j] * dp[i - j - 1] * 2;
+        }
+        if(i%2)
+            dp[i] -= dp[i/2] * dp[i/2];
+    }
+
+    cout<<dp[n];
+
+    return 0;
+}
