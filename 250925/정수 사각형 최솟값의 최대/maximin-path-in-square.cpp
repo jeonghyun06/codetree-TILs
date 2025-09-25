0c0bb47a319e371a30631ef0423746b8a9d8fa7e
LeeJeongHyeon <76839646+jeonghyun06@users.noreply.github.com>
2025-09-25 11:35:17 +0900

    Title: 정수 사각형 최솟값의 최대, Time: 11ms, Memory: 0MB, Status: Passed - Codetree

--- 250925/정수 사각형 최솟값의 최대/maximin-path-in-square.cpp
+++ 250925/정수 사각형 최솟값의 최대/maximin-path-in-square.cpp
@@ -8,25 +8,24 @@ int grid[100][100];
 int main() {
     cin >> n;
 
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= n; j++) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
             cin >> grid[i][j];
         }
     }
 
-    grid[0][1] = grid[1][0] = grid[1][1];
-    for(int i = 2; i <= n; i++){
-        grid[0][i] = min(grid[1][i - 1], grid[1][i]);
-        grid[i][0] = min(grid[i - 1][1], grid[i][1]);
+    for(int i = 1; i < n; i++){
+        grid[0][i] = min(grid[0][i - 1], grid[0][i]);
+        grid[i][0] = min(grid[i - 1][0], grid[i][0]);
     }
 
-    for(int i = 1; i <= n; i++){
-        for(int j = 1; j <= n; j++){
+    for(int i = 1; i < n; i++){
+        for(int j = 1; j < n; j++){
             grid[i][j] = min(max(grid[i-1][j], grid[i][j-1]), grid[i][j]);
         }
     }
     
-    cout<<grid[n][n];
+    cout<<grid[n-1][n-1];
 
     return 0;
 }
